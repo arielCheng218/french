b02ab809186250ec7bdb08478df2fe8117639ef6
Ariel <Cheng>
2021-12-22 15:01:00 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+
+<body>
+	<main>
+		<h1>mon blog de français</h1>
+		Bonjour
+	</main>
+</body>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		top: 40px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ffffff;
+		text-align: center;
+		font-size: 22px;
+		font-weight: 700;
+	}
+
+</style>
